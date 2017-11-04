68ea04554000d12fa7544ad0d63b3b4146eb2ad3
TheBedUnder <alexct96@yahoo.com>
2017-11-04 14:50:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.jme3.app.SimpleApplication;
+import com.jme3.math.Vector3f;
+
+
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+    }
+}
